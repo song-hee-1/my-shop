82c6c6d80b5764b750d7fbbe572bdae15e86b62d
hee <98_han@naver.com>
2023-08-25 15:49:25 +0900

    [refactor/accounts] unused import statement

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import json
-import os
 from pathlib import Path
-
-from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
