48a6801353f514aa350e695e53588f86d12d4662
hee <98_han@naver.com>
2023-08-25 15:31:06 +0900

    Feat : Core 앱 생성 및 base_service, custom renders

--- config/settings/base.py
+++ config/settings/base.py
@@ -134,3 +134,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST_FRAMEWORK settings
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'core.utils.renders.CustomRender',
+    ]
+}
