bbb6b328aefa646b28911b83de67b36bf459cb99
hee <98_han@naver.com>
2023-08-25 15:08:24 +0900

    [feature/accounts] 암호화 알고리즘 PBKDF2 > Argon2

--- config/settings/base.py
+++ config/settings/base.py
@@ -113,6 +113,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
