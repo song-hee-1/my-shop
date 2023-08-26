8f19422a5e3dad0c919f0a9c92fa2139c8b3df3b
hee <98_han@naver.com>
2023-08-26 12:52:54 +0900

    [feature/products] food > product 모델링 수정 및 status 추가 - soft delete를 위해 status 추가

--- products/models.py
+++ products/models.py
@@ -6,13 +6,19 @@ from core.models import TimeStampModel
 from core.utils.choice import get_choices
 
 
-class FoodSizeType(Enum):
+class ProductSizeType(Enum):
     UNDEFINED = 'undefined'
     SMALL = 'small'
     LARGE = 'large'
 
 
-class Food(TimeStampModel):
+class ProductStatusType(Enum):
+    UNDEFINED = 'undefined'
+    REGISTERED = 'registered'
+    DELETED = 'deleted'
+
+
+class Product(TimeStampModel):
     user = models.ForeignKey('accounts.User', related_name='foods', on_delete=models.CASCADE)
     price = models.PositiveIntegerField(help_text='가격')
     origin_price = models.PositiveIntegerField(help_text='원가')
@@ -21,11 +27,14 @@ class Food(TimeStampModel):
     barcode = models.CharField(max_length=100, help_text='바코드')
     expired_date = models.DateField(help_text='유통 기한')
     size = models.CharField(
-        choices=get_choices(FoodSizeType), default=FoodSizeType.UNDEFINED, max_length=30, help_text='사이즈'
+        choices=get_choices(ProductSizeType), default=ProductSizeType.UNDEFINED, max_length=30, help_text='사이즈'
+    )
+    status = models.CharField(
+        choices=get_choices(ProductStatusType), default=ProductStatusType.UNDEFINED, max_length=30, help_text='상태'
     )
 
 
-class FoodCategory(TimeStampModel):
+class ProductCategory(TimeStampModel):
     name = models.CharField(max_length=40, help_text='카테고리 이름')
     code = models.IntegerField(unique=True, null=True, default=None, help_text='카테고리 코드')
-    foods = models.ManyToManyField(Food, related_name='categories', help_text='상품')
+    product = models.ManyToManyField(Product, related_name='categories', help_text='상품')
