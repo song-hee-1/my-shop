ee4b60789e54267f6b7a4ba25e69fb09f858172d
hee <98_han@naver.com>
2023-08-27 16:19:20 +0900

    Feat : mysql docker container

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,13 +84,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
--- config/settings/local.py
+++ config/settings/local.py
@@ -1,8 +1,20 @@
-from config.settings.secrets import get_secrets
 from config.settings.base import *
+from config.settings.secrets import get_secrets
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = get_secrets('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_secrets('DATABASE_NAME'),
+        'USER': get_secrets('DATABASE_USER'),
+        'PASSWORD': get_secrets('DATABASE_PASSWORD'),
+        'HOST': get_secrets('DATABASE_HOST'),
+        'PORT': get_secrets('DATABASE_PORT'),
+    }
+}
